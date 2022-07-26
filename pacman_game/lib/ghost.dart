ce7c95330e63e4721cdd3b76897735218c732625
Tolga <tolgasuludere@gmail.com>
2022-07-26 15:39:28 +0300

    PacMan Game with Flutter (Demo)

---
+++ pacman_game/lib/ghost.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class MyGhost extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(12.0),
+      child: Image.asset('lib/images/ghost.png'),
+    );
+  }
+}
